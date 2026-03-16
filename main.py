a89148a650f0ef1b3a1654736ddf72dbded7dcac
elvisviss <elvis.elo97@gmail.com>
2026-03-16 20:34:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,21 @@ def sestej(a, b):
 def odstej(a, b):
     return a - b
 
+def pomnozi(a, b):
+    return a * b
+
+def deli(a, b):
+    if b == 0:
+        return "Napaka: Deljenje z nič ni dovoljeno!"
+    return a / b
+
 print("Pozdrav! Mini kalkulator")
 print("1 = seštevanje")
 print("2 = odštevanje")
+print("3 = množenje")
+print("4 = deljenje")
 
-izbira = input("Kaj želiš narediti? (1 ali 2): ")
+izbira = input("Kaj želiš narediti? (1, 2, 3 ali 4): ")
 
 if izbira == "1":
     x = float(input("Prvo število: "))
@@ -18,5 +28,13 @@ elif izbira == "2":
     x = float(input("Prvo število: "))
     y = float(input("Drugo število: "))
     print(f"Rezultat: {odstej(x, y)}")
+elif izbira == "3":
+    x = float(input("Prvo število: "))
+    y = float(input("Drugo število: "))
+    print(f"Rezultat: {pomnozi(x, y)}")
+elif izbira == "4":
+    x = float(input("Prvo število: "))
+    y = float(input("Drugo število: "))
+    print(f"Rezultat: {deli(x, y)}")
 else:
     print("Neveljavna izbira!")
